e649c79398297c7b3180a37daa22e62bfbf50114
Muhamed Spahić <muhamed1ism@gmail.com>
2025-04-22 19:22:18 +0200

    [BT-50] Add Get Current User Functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppConfigService } from 'config/service/appConfig.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService, AppConfigService],
 })
---
+++ src/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from 'prisma/generated/user/entities';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Get('current-user')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
